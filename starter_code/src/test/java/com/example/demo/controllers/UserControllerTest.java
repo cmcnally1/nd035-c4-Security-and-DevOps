28018ff952db296b14d500b53318911d16efa9ed
Ciaran McNally <ciaran.mcnally94@gmail.com>
2022-02-09 19:08:14 +0000

    Implemented find by id, find by username and user not found response tests

--- starter_code/src/test/java/com/example/demo/controllers/UserControllerTest.java
+++ starter_code/src/test/java/com/example/demo/controllers/UserControllerTest.java
@@ -5,10 +5,13 @@ import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.repositories.CartRepository;
 import com.example.demo.model.persistence.repositories.UserRepository;
 import com.example.demo.model.requests.CreateUserRequest;
+import net.bytebuddy.dynamic.DynamicType;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Optional;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
@@ -79,6 +82,72 @@ public class UserControllerTest {
 
         assertNotNull(response);
         assertEquals(400, response.getStatusCodeValue());
+
+    }
+
+    /**
+     * Happy path test for finding a user by ID
+     * @throws Exception
+     */
+    @Test
+    public void findById_happyPathTest() throws Exception {
+        User user = new User();
+        user.setId(0);
+        user.setUsername("test");
+        user.setPassword("testPassword");
+
+        when(userRepository.findById(0L)).thenReturn(Optional.of(user));
+
+        final ResponseEntity<User> response = userController.findById(0L);
+
+        assertEquals(200, response.getStatusCodeValue());
+
+        User foundUser = response.getBody();
+
+        assertNotNull(foundUser);
+        assertEquals(0, foundUser.getId());
+        assertEquals("test", foundUser.getUsername());
+        assertEquals("testPassword", foundUser.getPassword());
+
+    }
+
+    /**
+     * Happy path test for finding a user by Username
+     * @throws Exception
+     */
+    @Test
+    public void findByUserName_happyPathTest() throws Exception {
+        User user = new User();
+        user.setId(0);
+        user.setUsername("test");
+        user.setPassword("testPassword");
+
+        when(userRepository.findByUsername("test")).thenReturn(user);
+
+        final ResponseEntity<User> response = userController.findByUserName("test");
+
+        assertEquals(200, response.getStatusCodeValue());
+
+        User foundUser = response.getBody();
+
+        assertNotNull(foundUser);
+        assertEquals(0, foundUser.getId());
+        assertEquals("test", foundUser.getUsername());
+        assertEquals("testPassword", foundUser.getPassword());
+
+    }
+
+    /**
+     * Test that the correct response is returned when a user is not found
+     * @throws Exception
+     */
+    @Test
+    public void findByUserName_userNotFoundTest() throws Exception {
+
+        final ResponseEntity<User> response = userController.findByUserName("notAUser");
+
+        assertNotNull(response);
+        assertEquals(404, response.getStatusCodeValue());
 
     }
 }
